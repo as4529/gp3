fixed bug in marginal calculation for laplace partial grid

gp3/inference/laplace.py
@@ -313,7 +313,7 @@ class Laplace:
             like = np.sum(self.likelihood.log_like(f_lim, self.y))
             like = np.where(np.isnan(like), np.zeros_like(like), like)
 
-            return pen+eigs+like
+            return -(pen+eigs+like)
 
         pen = -0.5 * np.sum(np.multiply(self.alpha,
                                    self.f - self.mu))
@@ -408,7 +408,6 @@ class Laplace:
         obs_f = self.f[self.obs_idx]
         obs_grad = self.grad_func(obs_f, self.y)
         obs_hess = self.hess_func(obs_f, self.y)
-        self.obs_hess = obs_hess
 
         agg_grad = np.zeros(self.n, np.float64)
         agg_hess = np.zeros(self.n, np.float64)

gp3/inference/svi.py
@@ -429,6 +429,8 @@ class FullSVI(SVIBase):
 
         self.Rs = self.initialize_Rs_prior()
         self.trace_term, self.traces = self.calc_trace_term()
+        self.v_mu, self.v_k, self.m_mu, self.m_k = (None for _ in range(6))
+        self.v_s, self.m_s = [], []
 
     def run(self, its):
         """
@@ -449,9 +451,11 @@ class FullSVI(SVIBase):
             r = self.q_mu + kron_mvp(self.Rs, eps)
             like_grad_R, like_grad_mu = self.grad_like(r, eps)
 
-            grad_R = [np.clip(-KL_grad_R[i] + like_grad_R[i], -1e3, 1e3)
+            grad_R = [np.clip(-KL_grad_R[i] + like_grad_R[i], -self.max_grad,
+                              self.max_grad)
                       for i in range(len(KL_grad_R))]
-            grad_mu= np.clip(-KL_grad_mu + like_grad_mu, -1e3, 1e3)
+            grad_mu= np.clip(-KL_grad_mu + like_grad_mu, -self.max_grad,
+                             self.max_grad)
             R_and_grads = zip(grad_R, self.Rs)
             mu_and_grad = (grad_mu, self.q_mu)
 